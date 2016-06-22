605e8267af0bfbb2708165e3d85984ac16542820
Sai Susarla <sai.susarla@gmail.com>
2016-06-22 12:38:22 +0530

    Separate page annotations into a separate MongoDB collection

--- bookManager/__init__.py
+++ bookManager/__init__.py
@@ -13,9 +13,10 @@ from gridfs import GridFS
 from gridfs.errors import NoFile
 
 class Books:
-    def __init__(self, docdb):
+    def __init__(self, indicdocs):
         print "Initializing books collection ..."
-        self.books = docdb['books']
+        self.indicdocs = indicdocs
+        self.books = indicdocs.db['books']
 
     def insert(self, book):
         self.books.update({"path" : book["path"]}, book, upsert=True)
@@ -26,8 +27,12 @@ class Books:
         matches = [b for b in cursor if not pattern or re.search(pattern, b.name)]
         return matches
 
-    def getPage(self, book, idx):
-        return self.book.pages[idx]
+    def getPageByIdx(self, book, idx):
+        return book.pages[idx]
+
+    def getPageByName(self, book, pagename):
+        page = book.find({"pages.fname", name})
+        return page
 
     def get(self, path):
         binfo = self.books.find_one({'path' : path})
@@ -57,11 +62,54 @@ class Books:
                     book = json.load(fhandle)
                     #print json.dumps(book, indent=4)
                     book["path"] = bpath
+                    pgidx = 0
+                    for page in book['pages']:
+                        try:
+                            anno_id = self.indicdocs.annotations.insert( \
+                                { 'bpath' : bpath, 'pgidx' : pgidx, \
+                                   'anno' : [] })
+                            sec_id = self.indicdocs.sections.insert( \
+                                { 'bpath' : bpath, 'pgidx' : pgidx, \
+                                   'sections' : [] })
+                            #print "anno: " + str(anno_id) + ", sec: " + str(sec_id)
+                            page['anno'] = anno_id
+                            page['sections'] = sec_id
+                        except Exception as e:
+                            print "Error inserting anno", e
+                        #print json.dumps(page, indent=4)
+                        pgidx = pgidx + 1
+                    #print json.dumps(book, indent=4)
                     self.insert(book)
                     nbooks = nbooks + 1
             except Exception as e:
                 print "Skipped book " + f + ". Error:", e
         return nbooks
+
+class Annotations:
+    def __init__(self, docdb):
+        self.annotations = docdb['annotations']
+    def get(self, anno_id):
+        return self.annotations.find_one({'_id' : anno_id})
+    def insert(self, anno):
+        result = self.annotations.insert_one(anno)
+        objid = result.inserted_id
+        return objid
+    def update(self, anno_id, anno):
+        result = self.annotations.update({'_id' : anno_id}, anno)
+        return result.matched_count > 0
+
+class Sections:
+    def __init__(self, docdb):
+        self.sections = docdb['sectations']
+    def get(self, sec_id):
+        return self.sections.find_one({'_id' : sec_id})
+    def insert(self, sec):
+        result = self.sections.insert_one(sec)
+        objid = result.inserted_id
+        return objid
+    def update(self, sec_id, section):
+        result = self.sections.update({'_id' : sec_id}, section)
+        return result.matched_count > 0
 
 class Users:
     def __init__(docdb):
@@ -69,32 +117,29 @@ class Users:
 
 class IndicDocs:
     def __init__(self, dbname):
+        self.dbname = dbname
+        self.initialize()
+#        if not database.write_concern.acknowledged:
+#            raise ConfigurationError('database must use '
+#                                     'acknowledged write_concern')
+
+    def initialize(self):
         try:
-            self.dbname = dbname
             self.client = MongoClient()
             self.db = self.client[self.dbname]
+            self.books = Books(self)
+            self.annotations = Annotations(self.db)
+            self.sections = Sections(self.db)
             if not isinstance(self.db, Database):
                 raise TypeError("database must be an instance of Database")
         except Exception as e:
             print("Error initializing MongoDB database", e)
             sys.exit(1)
 
-#        if not database.write_concern.acknowledged:
-#            raise ConfigurationError('database must use '
-#                                     'acknowledged write_concern')
-
-        self.books = Books(self.db)
-        #self.annotations = self.db['annotations']
-
-        #self.__database = database
-        #self.__literals = database[collection]
-        #self.__fs = GridFS(database) 
-
     def reset(self):
         print "Clearing IndicDocs database"
         self.client.drop_database(self.dbname)
-        self.db = self.client[self.dbname]
-        self.books = Books(self.db)
+        self.initialize()
         
 #    def exists(self, document_or_id=None, **kwargs):
 #        return self.__fs.exists(document_or_id, **kwargs) 
