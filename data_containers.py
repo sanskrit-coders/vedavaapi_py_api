d3747c2d822e8779da8f3dbde19b399a7de2cec8
vishvAsaH <vishvas.vasuki@gmail.com>
2017-04-18 17:47:40 -0700

    Simplify serialization/ deserialization logic.

--- data_containers.py
+++ data_containers.py
@@ -11,43 +11,42 @@ logging.basicConfig(
 
 TYPE_FIELD = "py/object"
 
-class JsonObject:
+class JsonObject(object):
   def __init__(self):
+    self.set_type()
+
+  def set_type(self):
     # self.class_type = str(self.__class__.__name__)
-    setattr(self, TYPE_FIELD, self.__class__.__name__)
+    setattr(self, TYPE_FIELD, self.__module__ + "." + self.__class__.__name__)
+    # setattr(self, TYPE_FIELD, self.__class__.__name__)
+
+  def set_type_recursively(self):
+    self.set_type()
+    for key, value in self.__dict__.iteritems():
+      if isinstance(value, JsonObject):
+        value.set_type_recursively()
+      elif isinstance(value, list):
+        for item in value:
+          if isinstance(item, JsonObject):
+            item.set_type_recursively()
 
   def __str__(self):
     return str(self.__dict__)
 
-  # This does not work as of 20170418
-  @classmethod
-  def make_from_dict_jsonpickle(cls, input_dict):
-    obj = jsonpickle.decode(json_util.dumps(input_dict))
-    logging.info(obj)
-
   @classmethod
   def make_from_dict(cls, input_dict):
-    obj = JsonObject()
-    logging.debug(input_dict)
-    if input_dict.get(TYPE_FIELD) == "Target":
-      obj = Target()
-    elif input_dict.get(TYPE_FIELD) == "ImageTarget":
-      obj = ImageAnnotation.ImageTarget()
-    elif input_dict.get(TYPE_FIELD) == "Source":
-      obj = Annotation.Source()
-    elif input_dict.get(TYPE_FIELD) == "BookPortion":
-      obj = BookPortion()
-    elif input_dict.get(TYPE_FIELD) == "Annotation":
-      obj = Annotation()
-    elif input_dict.get(TYPE_FIELD) == "ImageAnnotation":
-      obj = ImageAnnotation()
-    elif input_dict.get(TYPE_FIELD) == "TextAnnotation":
-      obj = TextAnnotation()
-    else:
-      logging.error("Unknown TYPE_FIELD " + input_dict.get(TYPE_FIELD))
-    obj.set_from_dict(input_dict)
+    dict_without_id = input_dict
+    _id = dict_without_id.pop("_id", None)
+    obj = jsonpickle.decode(json_util.dumps(dict_without_id))
+    obj._id = _id
+    obj.set_type_recursively()
+    logging.info(obj)
     return obj
 
+  @classmethod
+  def make_from_pickledstring(cls, pickle):
+    obj = jsonpickle.decode(pickle)
+    return obj
 
   def set_from_dict(self, input_dict):
     for key, value in input_dict.iteritems():
@@ -121,32 +120,33 @@ class BookPortion(JsonObject):
     book_portion.set_from_dict(collection.find_one({"path" : path}))
     return book_portion
 
-class Annotation(JsonObject):
-  class Source(JsonObject):
-    @classmethod
-    def from_details(cls, type, id):
-      source = Annotation.Source()
-      source.type = type
-      source.id = id
-      return source
+class AnnotationSource(JsonObject):
+  @classmethod
+  def from_details(cls, type, id):
+    source = AnnotationSource()
+    source.type = type
+    source.id = id
+    return source
 
+
+class Annotation(JsonObject):
   def set_base_details(self, targets, source):
     self.targets = targets
     self.source = source
 
 
-class ImageAnnotation(Annotation):
-  class ImageTarget(Target):
-    @classmethod
-    def from_details(cls, container_id, x1=-1, y1=-1, x2=-1, y2=-1):
-      target = ImageAnnotation.ImageTarget()
-      target.container_id = container_id
-      target.x1 = x1
-      target.y1 = y1
-      target.x2 = x2
-      target.y2 = y2
-      return target
+class ImageTarget(Target):
+  @classmethod
+  def from_details(cls, container_id, x1=-1, y1=-1, x2=-1, y2=-1):
+    target = ImageTarget()
+    target.container_id = container_id
+    target.x1 = x1
+    target.y1 = y1
+    target.x2 = x2
+    target.y2 = y2
+    return target
 
+class ImageAnnotation(Annotation):
   @classmethod
   def from_details(cls, targets, source):
     annotation = ImageAnnotation()
--- data_containers_test.py
+++ data_containers_test.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
 import unittest
 
@@ -16,28 +17,42 @@ logging.basicConfig(
 class TestDBRoundTrip(unittest.TestCase):
   test_db = IndicDocs("test_db")
 
+  def test_PickleDepickle(self):
+    book_portion = data_containers.BookPortion.from_details(
+      title="halAyudhakoshaH", authors=["halAyudhaH"], path="myrepo/halAyudha", targets=[data_containers.Target.from_details(container_id="xyz")])
+    json_str = jsonpickle.encode(book_portion)
+    logging.info("json_str pickle is " + json_str)
+    obj = data_containers.JsonObject.make_from_pickledstring(json_str)
+    logging.info(obj.__class__)
+    logging.info(obj)
+
+    jsonMap = {u'py/object': u'data_containers.BookPortion', u'title': u'halAyudhakoshaH', u'path': u'myrepo/halAyudha', u'targets': [{u'py/object': u'data_containers.Target', u'container_id': u'xyz'}]}
+    json_str = json.dumps(jsonMap)
+    logging.info("json_str pickle is " + json_str)
+    obj = data_containers.JsonObject.make_from_pickledstring(json_str)
+    logging.info(obj.__class__)
+    logging.info(obj)
+
   def test_BookPortion(self):
     book_portion = data_containers.BookPortion.from_details(
       title="halAyudhakoshaH", authors=["halAyudhaH"], path="myrepo/halAyudha", targets=[data_containers.Target.from_details(container_id="xyz")])
-
-    json = jsonpickle.encode(book_portion)
-    logging.info("json pickle is " + json)
 
     book_portions = self.test_db.db.book_portions
     logging.debug(book_portion.toJsonMap())
     result = book_portions.update({"path" : book_portion.path}, book_portion.toJsonMap(), upsert=True)
     logging.debug("update result is "  + str(result))
 
-    book_portion_retrieved = data_containers.BookPortion.from_path(collection=book_portions, path="myrepo/halAyudha")
+    book_portion_retrieved = data_containers.JsonObject.make_from_dict(book_portions.find_one({"path" : "myrepo/halAyudha"}))
+    logging.info(book_portion_retrieved.__class__)
     logging.info(str(book_portion_retrieved.toJsonMap()))
-    logging.info(str(book_portion.toJsonMap()))
+    logging.info(book_portion.toJsonMap())
     self.assertTrue(book_portion.equals_ignore_id(book_portion_retrieved))
 
   def test_ImageAnnotation(self):
     target_page_id = ObjectId()
     annotation = data_containers.ImageAnnotation.from_details(targets=[
-      data_containers.ImageAnnotation.ImageTarget.from_details(container_id=target_page_id)],
-    source=data_containers.Annotation.Source.from_details("someProgram", "xyz.py"))
+      data_containers.ImageTarget.from_details(container_id=str(target_page_id))],
+    source=data_containers.AnnotationSource.from_details("someProgram", "xyz.py"))
 
     annotations = self.test_db.db.annotations
     logging.debug(annotation.toJsonMap())
@@ -46,6 +61,7 @@ class TestDBRoundTrip(unittest.TestCase):
     logging.debug("update result is "  + str(result))
 
     annotation_retrieved = data_containers.JsonObject.make_from_dict(annotations.find_one(annotation.toJsonMap()))
+    logging.info(annotation_retrieved.__class__)
 
     logging.info(str(annotation_retrieved.toJsonMap()))
     self.assertTrue(annotation.equals_ignore_id(annotation_retrieved))
@@ -53,8 +69,8 @@ class TestDBRoundTrip(unittest.TestCase):
   def test_TextAnnotation(self):
     target_image_id = ObjectId()
     annotation = data_containers.TextAnnotation.from_details(targets=[
-      data_containers.Target.from_details(container_id=target_image_id)],
-      source=data_containers.Annotation.Source.from_details("someOCRProgram", "xyz.py"), content=data_containers.TextContent.from_details(u"इदं नभसि म्भीषण"))
+      data_containers.Target.from_details(container_id=str(target_image_id))],
+      source=data_containers.AnnotationSource.from_details("someOCRProgram", "xyz.py"), content=data_containers.TextContent.from_details(u"इदं नभसि म्भीषण"))
 
     annotations = self.test_db.db.annotations
     logging.debug(annotation.toJsonMap())
