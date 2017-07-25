53ea6b64432bf842dac75241589d564d632eb587
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-24 18:25:36 -0700

    Update docs.

--- ullekhanam/api_v1.py
+++ ullekhanam/api_v1.py
@@ -18,8 +18,9 @@ URL_PREFIX = '/v1'
 api_blueprint = Blueprint(name='ullekhanam_api', import_name=__name__)
 api = flask_restplus.Api(app=api_blueprint, version='1.0', title='vedavaapi py ullekhanam API',
                          description='For detailed intro and to report issues: see <a href="https://github.com/vedavaapi/vedavaapi_py_api">here</a>. '
+                                     'For using some API, you need to log in using <a href="../oauth/login/google">google</a>.'
                                      'For a list of JSON schema-s this API uses (referred to by name in docs) see <a href="v1/schemas"> here</a>. <BR>'
-                                     'A list of REST and non-REST API routes avalilable on this server: <a href="../sitemap">sitemap</a>.  ',
+                                     'A list of REST and non-REST API routes avalilable on this server: <a href="../sitemap">sitemap</a>. ',
                          default_label=api_blueprint.name,
                          prefix=URL_PREFIX, doc='/docs')
 
