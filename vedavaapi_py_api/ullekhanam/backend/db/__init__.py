d2a3e522ec41165291918f3a3349e9ee221e2560
vishvAsaH <vishvas.vasuki@gmail.com>
2017-11-10 12:07:34 -0800

    Move index creation code to schema files.

--- vedavaapi_py_api/ullekhanam/backend/db/__init__.py
+++ vedavaapi_py_api/ullekhanam/backend/db/__init__.py
@@ -4,7 +4,8 @@ import logging
 
 from cloudant.database import CouchDatabase
 from pymongo.collection import Collection
-
+from sanskrit_data.schema.ullekhanam import TextAnnotation
+from sanskrit_data.schema.books import BookPortion
 from vedavaapi_py_api.ullekhanam.backend.db import collections
 
 dbs = {}
@@ -20,9 +21,19 @@ def add_db(db, db_name="ullekhanam"):
   elif isinstance(db, Collection):
     from vedavaapi_py_api.ullekhanam.backend.db.collections import BookPortionsMongodb
     ullekhanam_db = BookPortionsMongodb(db)
-  ullekhanam_db.add_index(keys_dict={
-    "targets.container_id": 1
-  }, index_name="targets_container_id")
+
+  ## General indices (corresponds to schema defined in schema.common).
+  # Not really required since BookPortion index creation automatically includes the below.
+  # JsonObject.add_indexes(db_interface=ullekhanam_db)
+  # JsonObjectWithTarget.add_indexes(db_interface=ullekhanam_db)
+
+  ## Book portion indices (corresponds to schema defined in schema.books).
+  # Appropriate index use confirmed: https://trello.com/c/CHKgDABv/117-mongodb-index-array-of-array
+  BookPortion.add_indexes(db_interface=ullekhanam_db)
+
+  ## Annotation indices (corresponds to schema defined in schema.ullekhanam).
+  TextAnnotation.add_indexes(db_interface=ullekhanam_db)
+
   global dbs
   dbs[db_name] = ullekhanam_db
 
