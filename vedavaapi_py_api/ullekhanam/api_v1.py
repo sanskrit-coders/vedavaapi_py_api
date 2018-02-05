9ee95b6d883f3e21ab6836123f6f74245677a93a
vishvAsaH <vishvas.vasuki@gmail.com>
2018-02-05 13:48:45 -0800

    Fix schema API link

--- vedavaapi_py_api/ullekhanam/api_v1.py
+++ vedavaapi_py_api/ullekhanam/api_v1.py
@@ -17,7 +17,8 @@ api_blueprint = Blueprint(name='ullekhanam_api', import_name=__name__)
 api = flask_restplus.Api(app=api_blueprint, version='1.0', title='vedavaapi py ullekhanam API',
                          description='For detailed intro and to report issues: see <a href="https://github.com/vedavaapi/vedavaapi_py_api">here</a>. '
                                      'For using some API, you need to log in using <a href="../auth/v1/oauth_login/google">google</a>.'
-                                     'For a list of JSON schema-s this API uses (referred to by name in docs) see <a href="v1/schemas"> here</a>. '
+                         # We are not linking to  <a href="v1/schemas"> below since it results in an error on Chrome. See https://github.com/vedavaapi/vedavaapi_py_api/issues/3 
+                                     'For a list of JSON schema-s this API uses (referred to by name in docs) see the schemas API below.</a>. '
                                      'Please also see videos <a href="https://www.youtube.com/playlist?list=PL63uIhJxWbghuZDlqwRLpPoPPFDNQppR6">here</a>, <a href="http://sanskrit-data.readthedocs.io/en/latest/sanskrit_data_schema.html#class-diagram" > class diagram </a> as well as the sources ( <a href="http://sanskrit-data.readthedocs.io/en/latest/_modules/sanskrit_data/schema/books.html#BookPortion">example</a> ) - It might help you understand the schema more easily.<BR>'
                                      'A list of REST and non-REST API routes avalilable on this server: <a href="../sitemap">sitemap</a>. ',
                          default_label=api_blueprint.name,
--- vedavaapi_py_api/users/api_v1.py
+++ vedavaapi_py_api/users/api_v1.py
@@ -24,7 +24,7 @@ api_blueprint = Blueprint(
 
 api = flask_restplus.Api(app=api_blueprint, version='1.0', title='vedavaapi py users API',
                          description='For detailed intro and to report issues: see <a href="https://github.com/vedavaapi/vedavaapi_py_api">here</a>. '
-                                     'For a list of JSON schema-s this API uses (referred to by name in docs) see <a href="schemas"> here</a>. <BR>'
+                                     'For a list of JSON schema-s this API uses (referred to by name in docs) see <a href="v1/schemas"> here</a>. <BR>'
                                      'Please also see the <a href="http://sanskrit-data.readthedocs.io/en/latest/sanskrit_data_schema.html#class-diagram" > class diagram </a> as well as the sources ( <a href="http://sanskrit-data.readthedocs.io/en/latest/_modules/sanskrit_data/schema/books.html#BookPortion">example</a> ) - It might help you understand the schema more easily.<BR>'
                                      'A list of REST and non-REST API routes avalilable on this server: <a href="../sitemap">sitemap</a>.',
                          default_label=api_blueprint.name,
